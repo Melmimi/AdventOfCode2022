2dd4a0a666a1e2ea4728c64d180048b1e0bb92e4
Melmimi <melmidiezweite@yahoo.de>
2022-11-30 23:17:48 +0100

    trying to setup github desktop on laptop

---
+++ AdventOfCode2022/ImportString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    class Import
+    {
+        public static string ImportString()
+        {
+
+           string importString = System.IO.File.ReadAllText(@"C:\Users\miche\source\repos\AdventOfCode2022\InputFiles\Day_00.txt");
+            return importString;
+        }
+
+    }
+}
